c4b4cb472aea51fb0e91a8c27f2eb95603939ca4
Facet <facet@facetframer.com>
2017-03-09 00:13:40 +0000

    remove default snippets
    These potentiallyu get in the way of do what I mean snippets

--- snippets.plugin.zsh
+++ snippets.plugin.zsh
@@ -177,38 +177,3 @@ run-help-list-snippets(){
     help-list-snippets inZLE
 }
 zle -N run-help-list-snippets
-
-
-# set up some default snippets
-snippet-add l      "less "
-snippet-add tl     "| less "
-snippet-add g      "grep "
-snippet-add tg     "| grep "
-snippet-add gl     "grep -l"
-snippet-add tgl    "| grep -l"
-snippet-add gL     "grep -L"
-snippet-add tgL    "| grep -L"
-snippet-add gv     "grep -v "
-snippet-add tgv    "| grep -v "
-snippet-add eg     "egrep "
-snippet-add teg    "| egrep "
-snippet-add fg     "fgrep "
-snippet-add tfg    "| fgrep "
-snippet-add fgv    "fgrep -v "
-snippet-add tfgv   "| fgrep -v "
-snippet-add ag     "agrep "
-snippet-add tag    "| agrep "
-snippet-add ta     "| ag "
-snippet-add p      "${PAGER:-less} "
-snippet-add tp     "| ${PAGER:-less} "
-snippet-add h      "head "
-snippet-add th     "| head "
-snippet-add t      "tail "
-snippet-add tt     "| tail "
-snippet-add s      "sort "
-snippet-add ts     "| sort "
-snippet-add v      "${VISUAL:-${EDITOR:-nano}} "
-snippet-add tv     "| ${VISUAL:-${EDITOR:-nano}} "
-snippet-add tc     "| cut "
-snippet-add tu     "| uniq "
-snippet-add tx     "| xargs "
