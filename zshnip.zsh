f83fee26eaad41768ce1f1b7e39bd162d9171878
Facet <facet@facetframer.com>
2017-04-10 17:24:50 +0100

    Allow recursive definitino of snippets
    This might seem like feature creep.
    
    In practice this is one of the first things you start
    trying to do when using this library.

--- zshnip.zsh
+++ zshnip.zsh
@@ -5,36 +5,39 @@
 # use: add-snippet <key> <expansion>
 # then, with cursor just past <key>, run zshnip-expand
 
-zshnip_should_log= # empty to prevent logging
+zshnip_should_log= # non-empty to log
 
 snippets_file=~/.zshnip
 #snippet_edit_func=snippet-editor-edit
 zshnip_edit_func=zshnip-shell-edit
 
+set -A _zshnip_defining # Stack of snippets that we are defining
+set -A _zshnip_previous_lbuffer # Stack contents of zle buffer
+set -A _zshnip_previous_rbuffer # Stack contents of zle buffer
+
 typeset -Ag snippets
 
-zshnip-log() {
+_zshnip-log() {
     if [ -n "${zshnip_should_log:-}" ]; then
-        zshnip-warn "$*"
+        _zshnip-warn "$*"
     fi;
 }
 
-zshnip-warn() {
+_zshnip-warn() {
+    # Display a visible message
     echo >&2 "zshnip: $*"
 }
+
+_zshnip-log "Starting zshnip"
 
 zshnip-add() {
     # zshnip-add <key> <expansion>
     snippets[$1]="$2"
 }
 
-zshnip-exists() {
-    [ -n "${snippets[$1]:-}" ];
-}
-
 zshnip-editor-edit() {
     name="$1"
-    zshnip-log "Editing $name"
+    _zshnip-log "Editing $name"
     filename=$(mktemp)
     echo "$snippets[$snippet_match]" > "$filename"
     finished=$(mktemp --dry-run)
@@ -48,60 +51,70 @@ zshnip-editor-edit() {
     zshnip-write "$name" "$(cat $filename)"
 }
 
-zshnip-line-restore () {
-    LBUFFER="$CURRENT_SNIP_LINE"
-}
-zle -N zshnip-line-restore
-
-set -A zshnip_shell_defining
 zshnip-shell-edit() {
     emulate -L zsh
     setopt extendedglob
-    zshnip_shell_defining="$1"
-    zshnip_shell_before_snippet="$BUFFER"
-    zshnip-log "Setting buffer to expansion of $snippet_match"
-    new_buffer="$snippets[$1]"
-    BUFFER=$new_buffer
-    OLDPROMPT="$PROMPT"
-    PROMPT="$PROMPT DEFINING SNIPPET: $zshnip_shell_defining > "
+    _zshnip-log "Saving prompt: $PROMPT $LBUFFER ^ $RBUFFER"
+    _zshnip_previous_lbuffer+=( "$LBUFFER" )
+    _zshnip_previous_rbuffer+=( "$RBUFFER" )
+    _zshnip_previous_prompt+=( "$PROMPT" )
+    _zshnip_defining+=( "$1" )
+
+    _zshnip-log "Editing $snippet_match"
+    BUFFER="$snippets[$1]"
+    PROMPT="$PROMPT DEFINING SNIPPET: $1 > "
     zle reset-prompt
-    PROMPT="$OLDPROMPT"
-    zle -A accept-line _zshnip_old_accept_line
-    zle -N accept-line zshnip-shell-edit-finished
+    zle -A accept-line _zshnip_old_accept_line # Save previous accept line function
+    zle -N accept-line _zshnip-shell-edit-accept-line
 }
 zle -N zshnip-shell-edit
 
-zshnip-shell-edit-finished() {
+_zshnip-shell-edit-accept-line() {
+    _zshnip-log "Editing finished"
     emulate -L zsh
-    local new_snippet
-    local defining
-
-    defining="$zshnip_shell_defining"
-    zshnip_shell_defining=
-
-    zshnip-write "$defining" "$BUFFER"
+    local new_snippet defining previous_lbuffer previous_rbuffer
 
     zshnip-source
+    previous_lbuffer=$_zshnip_previous_lbuffer[-1]
+    previous_rbuffer=$_zshnip_previous_rbuffer[-1]
+    previous_prompt=$_zshnip_previous_prompt[-1]
+    defined=$_zshnip_defining[-1]
 
-    zle -N accept-line _zshnip_old_accept_line
-    LBUFFER="$zshnip_shell_before_snippet"
+    zshnip-write "$defined" "$BUFFER"
+
+    LBUFFER="$previous_lbuffer"
+    RBUFFER="$previous_rbuffer"
+    PROMPT="$previous_prompt"
+    _zshnip-log "Restored prompt: $PROMPT ${LBUFFER}_${RBUFFER}"
+
+    _zshnip_previous_lbuffer[-1]=()
+    _zshnip_previous_rbuffer[-1]=()
+    _zshnip_previous_prompt[-1]=()
+    _zshnip_defining[-1]=()
+
     zle reset-prompt
 
-    zshnip-edit-finished-callback
+    _zshnip-edit-finished-callback "$defined"
+
+    if [[ "0" == "${#_zshnip_defining}" ]]; then
+        zle -N accept-line _zshnip_old_accept_line
+    fi;
 }
 zle -N zshnip-shell-finished
 
 zshnip-source () {
+    # Reload the snippets file
     if [ -f "$snippets_file" ]; then
         source "$snippets_file"
     fi;
 }
 
 zshnip-write () {
+    # Save a snippet to permanent storage
     name=$1
     content=$2
 
-    zshnip-log "Setting snippet '$name' to '$content'"
+    _zshnip-log "Saving snippet '$name' -> '$content'"
 
     escaped_content=$(echo "$content" | sed "s/'/\\\\'/g" )
     echo zshnip-add "$name" $'$\''"$escaped_content""'" >> $snippets_file
@@ -110,30 +123,39 @@ zshnip-write () {
 zshnip-expand() {
     emulate -L zsh
     setopt extendedglob
-    zshnip-parse-snippet
+    _zshnip-parse-snippet
     LBUFFER=$snippet_new_lbuffer
     LBUFFER+="${snippets[$snippet_match]:-$snippet_match}"
 }
 zle -N zshnip-expand
 
-zshnip-string-expand () {
-    echo "${snippets[$snippet_match]:-$snippet_match}"
+_zshnip-edit() {
+    local editor snippet
+    editor=$1
+    snippet=$2
+    _zshnip_defining+=( $snippet )
+    $zshnip_edit_func $editor _zshnip-edit-finished-callback
 }
 
-
 zshnip-expand-or-edit() {
-    zshnip-parse-snippet
-    LBUFFER=$snippet_new_lbuffer
+    if [ -z "$BUFFER" ]; then
+        # Useful binding to re-edit the last snippet if it was not right
+        _zshnip-edit "$_zshnip_last_expanded"
+        return
+    fi;
+
+    _zshnip-parse-snippet
     if [ -z "${snippets[$snippet_match]:-}" ]; then
-        $zshnip_edit_func "$snippet_match"
+        _zshnip-edit "$snippet_match"
     else
-        zshnip-edit-finished-callback
+        _zshnip-expand "$snippet_match"
     fi;
 
 }
 zle -N zshnip-expand-or-edit
 
 zshnip-save-last() {
+    # Save the last command to a snippet
     name="$1"
     content="$(history | tail -n 1 | head -n 1 | cut -b 8-)"
     snippet-write "$name" "$content"
@@ -145,23 +167,45 @@ zshnip-edit-and-expand() {
 
     if [ -z "$BUFFER" ]; then
         # Useful binding to re-edit the last snippet if it was not right
-        $zshnip_edit_func "$snippet_match"
+        _zshnip-edit "$_zshnip_last_expanded"
         return
     fi;
 
-    zshnip-parse-snippet
+    _zshnip-parse-snippet
     LBUFFER=$snippet_new_lbuffer
-    $zshnip_edit_func "$snippet_match" zshnip-edit-finished-callback
+    _zshnip-edit "$snippet_match" _zshnip-edit-finished-callback
 }
 zle -N zshnip-edit-and-expand
 
-zshnip-edit-finished-callback() {
-    zshnip-log "Snippet edit finished"
+_zshnip-edit-finished-callback() {
+    local defined
+    defined=$1
+    _zshnip-log "Snippet edit finished"
     zshnip-source
-    LBUFFER+=${snippets[$snippet_match]:-$snippet_match}
+    defining=$_zshnip_
+    _zshnip-expand "$defined"
 }
 
-zshnip-parse-snippet(){
+_zshnip-expand () {
+    local snippet
+    snippet=$1
+    _zshnip-log "Expanding $snippet"
+    _zshnip-log "Buffer before expansion ${LBUFFER}_${RBUFFER}"
+    LBUFFER=${LBUFFER%$snippet}
+    LBUFFER+=${snippets[$snippet]:-$snippet}
+    _zshnip_last_expanded=$snippet
+    _zshnip-log "Buffer after expansion ${LBUFFER}_${RBUFFER}"
+}
+
+zshnip-enable-log() {
+    zshnip_should_log=1 # non-empty to log
+}
+
+zshnip-disable-log() {
+    zshnip_should_log= # non-empty to log
+}
+
+_zshnip-parse-snippet(){
     # Wouldn't it be great if we could return
     #   composite data types
     emulate -L zsh
@@ -169,7 +213,6 @@ zshnip-parse-snippet(){
 
     snippet_new_lbuffer=${LBUFFER%%(#m)[.\-+:|_a-zA-Z0-9]#}
     snippet_match=$MATCH
-    zshnip-log "Found snippet $snippet_match"
 }
 
 _zshnip-do-list(){
@@ -183,6 +226,7 @@ _zshnip-do-list(){
         echo "$help" | ${PAGER:-less}
     fi
 }
+
 zshnip-list(){
     _zshnip-do-list inZLE
 }
