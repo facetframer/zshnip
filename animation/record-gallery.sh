db75afeb5a5dfcf0683d8a5656408e3544b9c662
Facet <facet@facetframer.com>
2017-05-02 21:53:28 +0100

    "Fix" race condition in record-gallery.sh
    Fixed by slowing down floating of window.
    
    I think this race occurs because the operation of "floating"
    interferes with the operation of resizing that goes on when
    mate-terminal gets spawned.
    
    Delaying the floating seems to fix this.

--- animation/record-gallery.sh
+++ animation/record-gallery.sh
@@ -8,16 +8,9 @@ cd "$here"
 
 "$here/animate.sh" "Zshnip: Expanding a snippet" gallery-expand-actions.sh gallery-animations/gallery-expand.json
 
-sleep 1
-
 "$here/animate.sh" "Zshnip: Defining a new snippet" gallery-define-actions.sh gallery-animations/gallery-define.json
 
-
-sleep 1
-
 "$here/animate.sh" "Zshnip: Editing an existing snippet" gallery-edit-actions.sh gallery-animations/gallery-edit.json
-
-sleep 1
 
 "$here/animate.sh" "Zshnip: Defining multiple snippets at the same time" gallery-nest-actions.sh gallery-animations/gallery-nest.json
 @
--- animation/spawn-term-inner.sh
+++ animation/spawn-term-inner.sh
@@ -7,6 +7,8 @@ exec 2> /tmp/damnlog
 source "$here/util.sh"
 log "$0 started"
 
-i3-msg -t command "floating enable" > /dev/null;
+# hack to work with i3
+sleep 1;
+i3-msg -t command "floating enable" > /dev/null 2>&1 || true;
 
 "$@"
